cf0249ab749ee5372386d4c134916ba13268a6e8
Ali Atahan Topal <atahanali57@gmail.com>
2021-05-13 15:50:52 +0300

    popup ekran kapanmama olayı düzeltildi- alert eklendi.

--- Isik.SAMS/Controllers/ProgramController.cs
+++ Isik.SAMS/Controllers/ProgramController.cs
@@ -11,11 +11,51 @@ namespace Isik.SAMS.Controllers
     {
         // GET: Program
         StudentApprovalManagementEntities db = new StudentApprovalManagementEntities();
-        public ActionResult Index()
+        public ActionResult Index(string alertMessage)
         {
+            string message = alertMessage;
             var values = db.SAMS_Program.ToList();
+            if (message == "1")
+            {
+                ViewBag.Message = "Succesfully inserted.";
+            }
+            else if (message == "2")
+            {
+                ViewBag.Message = "Succesfully deleted.";
+            }
+            else if (message == "3")
+            {
+                ViewBag.Message = "Succesfully updated.";
+            }
+            else if (message == "4")
+            {
+                ViewBag.Message = "Operation failed.";
+            }
             return View(values);
         }
+
+        //public ActionResult Index(string message)
+        //{
+        //    var values = db.SAMS_Program.ToList();
+        //    if (message == "1")
+        //    {
+        //        ViewBag.Message = "Succesfully inserted.";
+        //    }
+        //    else if (message == "2")
+        //    {
+        //        ViewBag.Message = "Succesfully deleted.";
+        //    }
+        //    else if (message == "3")
+        //    {
+        //        ViewBag.Message = "Succesfully updated.";
+        //    }
+        //    else
+        //    {
+        //        ViewBag.Message = "Operation failed.";
+        //    }
+        //    return View(values);
+        //}
+
         [HttpGet]
         public ActionResult Insert()
         {
@@ -24,24 +64,27 @@ namespace Isik.SAMS.Controllers
         [HttpPost]
         public ActionResult Insert(SAMS_Program s1)
         {
+            string alertMessage;
             s1.CreatedBy = 1; // adminId when the session created
             s1.CreatedTime = DateTime.Now;
             db.SAMS_Program.Add(s1);
             db.SaveChanges();
+            alertMessage = "1";
+            return RedirectToAction("Index", new { alertMessage = alertMessage });
 
-            return View(); // RedirectToAction("Index");
         }
         public ActionResult Delete(int id)
         {
             var program = db.SAMS_Program.Find(id);
             db.SAMS_Program.Remove(program);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            string alertMessage = "2";
+            return RedirectToAction("Index", new { alertMessage = alertMessage });
         }
         [HttpGet]
         public ActionResult Update(int Id)
         {
-            var program = db.SAMS_Program.Find(Id);            
+            var program = db.SAMS_Program.Find(Id);
             return View(program);
         }
 
@@ -52,9 +95,8 @@ namespace Isik.SAMS.Controllers
             program.ProgramName = p1.ProgramName;
             program.CreatedTime = p1.CreatedTime;
             db.SaveChanges();
-            return RedirectToAction("Index");
-
-
+            string alertMessage = "3";
+            return RedirectToAction("Index", new { alertMessage = alertMessage });
         }
     }
 }
