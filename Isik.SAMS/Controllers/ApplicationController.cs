bbcc429443ce687fbc71ec52007d9d9802a23293
Ali Atahan Topal <atahanali57@gmail.com>
2021-06-05 23:28:51 +0300

    merged

---
+++ Isik.SAMS/Controllers/ApplicationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Isik.SAMS.Models.Entity;
+using System.Data;
+
+namespace Isik.SAMS.Controllers
+{
+
+    public class ApplicationController : Controller
+    {
+        StudentApprovalManagementEntities db = new StudentApprovalManagementEntities();
+        // GET: Application
+        public ActionResult Index()
+        {
+            var model = db.SAMS_StudentApplications.ToList();
+            return View(model);
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
