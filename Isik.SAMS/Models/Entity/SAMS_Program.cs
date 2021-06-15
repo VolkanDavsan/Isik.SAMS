87bf22fe2a607c6e208b8f1075a4d1279992ff26
Ali Atahan Topal <atahanali57@gmail.com>
2021-06-15 22:22:52 +0300

    final push

--- Isik.SAMS/Models/Entity/SAMS_Program.cs
+++ Isik.SAMS/Models/Entity/SAMS_Program.cs
@@ -23,10 +23,6 @@ namespace Isik.SAMS.Models.Entity
         }
     
         public int Id { get; set; }
-        public Nullable<int> CreatedBy { get; set; }
-        public Nullable<System.DateTime> CreatedTime { get; set; }
-        public Nullable<int> ChangedBy { get; set; }
-        public Nullable<System.DateTime> ChangedTime { get; set; }
         public string ProgramName { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
